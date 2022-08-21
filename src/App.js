3bad3269db3e678725eb17aa12963db8e29a2d41
Paul-M-Coorales <paulmatiascorrales@gmail.com>
2022-08-21 08:25:07 -0300

    prueba react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let imagen = "img/Smart Tv Hd 32.webp";
+  let titulo = "Smart TV HD 32” Philips 32PHD6825";
+  let precio = 46.999;
+  let imagenMarca = "img/Phillips.webp";
+  let marca = "Phillips";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+      <div className='col-md-4 offset-md-2'>
+        <img src={imagen} alt={titulo} className= "img-fluid"/>
+      </div>
+      <div className='col-md-4'>
+      <img src={imagenMarca} alt={marca} className= "img-fluid"/>
+        <p>{titulo}</p>
+        <p><b>${precio}</b></p>
+
+      </div>
+      </div>
     </div>
   );
 }
